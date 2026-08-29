7cff93aff3350249dd1e10791b1ceb16a33d882e
DemaFPS <dvorovds5@gmail.com>
2026-08-29 23:07:11 +0300

    Временное использование thingproxy

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const auth = firebase.auth();
 // ============================
 // 1. ПРОКСИ URL (ЗАМЕНИТЕ НА СВОЙ)
 // ============================
-const PROXY_URL = 'https://shrill-hall-edf3.dvorovds5.workers.dev';
+const PROXY_URL = 'https://thingproxy.freeboard.io/fetch/' + 'https://script.google.com/macros/s/AKfycbxQaM_bt7w4CBSM_zQuVhKIXetyhbgPESptufWTiubag2yjpu0QaCr-vvOE67grUvtK/exec';
 
 // ============================
 // 2. ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
