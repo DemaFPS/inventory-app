97725606e690cd67287b37cdfb08a597b86a6675
DemaFPS <dvorovds5@gmail.com>
2026-08-29 21:57:44 +0300

    test

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const auth = firebase.auth();
 // ============================
 // 1. ПРОКСИ URL (ЗАМЕНИТЕ НА СВОЙ)
 // ============================
-const PROXY_URL = 'https://script.google.com/macros/s/AKfycbzL6BujywVGTT1bX3U-z5uBZClWSKXD55_pz2D8gWK6JKa-jPJO-HaMjr3_tqEB7T42/exec';
+const PROXY_URL = 'https://script.google.com/macros/s/AKfycbx54pGkXLBBVMejqoYa6V-cKEGoWkIpW_fPXlVVHDkTZFDe2qoX0tOaPVWr0RkhzIUw/exec';
 
 // ============================
 // 2. ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
