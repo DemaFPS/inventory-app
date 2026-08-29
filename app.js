87e2e1bd67929f3d9ead98eb8b6402002a4fa0dc
DemaFPS <dvorovds5@gmail.com>
2026-08-29 23:30:45 +0300

    Переход на Cloudflare Worker (исправлен)

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const auth = firebase.auth();
 // ============================
 // 1. ПРОКСИ URL (ЗАМЕНИТЕ НА СВОЙ)
 // ============================
-const PROXY_URL = 'https://corsproxy.io/?key=b589d1d8&url=' + encodeURIComponent('https://script.google.com/macros/s/AKfycbxQaM_bt7w4CBSM_zQuVhKIXetyhbgPESptufWTiubag2yjpu0QaCr-vvOE67grUvtK/exec');
+const PROXY_URL = 'https://cors-proxy-2.dvorovds5.workers.dev';
 
 // ============================
 // 2. ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ
